e9fad199fc5a8110bde01fb66bdbc8153779505a
Salmonte <almontesauel@gmail.com>
2022-12-24 23:55:04 -0500

    Node JS API

--- controllers/post.js
+++ controllers/post.js
@@ -1,16 +1,18 @@
 const Post = require("../models/post")
 
 exports.getPosts = (req, res) => {
-    res.json({
-        posts: [{title: 'First Post'}, {title: 'Second Response'}]
-    })
+    const posts = Post.find().select('id title body')
+        .then(posts => {
+            res.json({posts})
+        })
+        .catch(err => console.log(err))
 }
 
 exports.createPost = (req, res) => {
     const post = new Post(req.body)
     // Refactored
     post.save().then(result => {
-        res.status(200).json({
+        res.json({
             post: result
         })
     })
